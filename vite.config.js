cce260f4657c15c3ad1a78db600d84141b16d065
ANDISAMY <ANDISAMY>
2025-07-05 14:45:22 +0530

    add files with new

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['uuid']
-    }
+  resolve: {
+    alias: {
+      uuid: "uuid/dist/esm-browser/index.js",
+    },
   },
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+});
