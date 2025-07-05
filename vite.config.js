5fff601f3fd7d6f7c427549f6e7daae1ad3e790a
ANDISAMY <ANDISAMY>
2025-07-05 13:53:34 +0530

    add files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['uuid']
+    }
+  },
   plugins: [react(),tailwindcss()],
 })
